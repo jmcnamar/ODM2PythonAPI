e32220c5810cbd384ac3adcca0e70ea2a2d1767f
valentinedwv <david.valentine@gmail.com>
2016-04-08 10:55:20 -0700

    remove datetutils dependency from setup and requirements

--- odm2api/ODM1_1_1/services/edit_service.py
+++ odm2api/ODM1_1_1/services/edit_service.py
@@ -1,4 +1,4 @@
-import sqlite3
+#import sqlite3
 
 #from ...versionSwitcher import ODM
 from series_service import SeriesService
--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
         'sqlalchemy',
         'geoalchemy>=0.7.3',
         'shapely',
-        'dateutils',
+#        'dateutils',
         'pandas',
     ],
     # dependency_links- geoalchemy from the ODM repository
